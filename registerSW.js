3971224450e97f39f48678a966d7baa96fc0504a
Michael Oesterle <michaeloesterle01@gmail.com>
2025-12-25 21:29:11 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/schompf-1/sw.js', { scope: '/schompf-1/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
